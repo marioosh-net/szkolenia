27f61462487b79db026f5193f4591657a2725ff5
marioosh <marioosh.net@gmail.com>
2011-10-13 09:17:44 +0200

    jms... / not working

---
+++ webapptest/src/main/java/myself/jms/JmsReceiver.java
@@ -0,0 +1,14 @@
+package myself.jms;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+
+public class JmsReceiver { /* implements MessageListener {
+
+	@Override
+	public void onMessage(Message msg) {
+		System.out.println(msg);
+	}*/
+
+}
---
+++ webapptest/src/main/java/myself/jms/JmsSender.java
@@ -0,0 +1,47 @@
+package myself.jms;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class JmsSender
+ */
+@WebServlet("/JmsSender")
+public class JmsSender extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Resource(mappedName = "QueueConnectionFactory")
+	private javax.jms.QueueConnectionFactory qcf;
+	@Resource(mappedName = "responsetest")
+	private Queue q;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		try {
+			QueueConnection c = qcf.createQueueConnection();
+			QueueSession s = c.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+			QueueSender sender = s.createSender(q);
+			TextMessage m = s.createTextMessage("hello");
+			sender.send(m);
+			// close na connection zamyka sesjê i sendera/receivera
+			c.close();
+			// zaleca siê róbiæ close
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
