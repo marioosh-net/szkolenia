840f3fd8ccf389f802a68d6e2f0038d0307dcd57
marioosh <marioosh.net@gmail.com>
2011-10-13 09:54:17 +0200

    EJB Interceptor

---
+++ webapptest/src/main/java/myself/ejb/interceptor/Interceptor.java
@@ -0,0 +1,22 @@
+package myself.ejb.interceptor;
+
+import java.util.Arrays;
+
+import javax.annotation.PostConstruct;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
+
+public class Interceptor {
+	
+	@AroundInvoke
+	public Object invoke(InvocationContext ctx) {
+		System.out.println("invoked: "+ctx.getTarget().getClass().getName()+
+				", "+ctx.getMethod().getName()+ " ("+Arrays.toString(ctx.getParameters())+")");
+		return null;
+	}
+	
+	@PostConstruct
+	private void create() {
+		System.out.println("Interceptor created");
+	}
+}
